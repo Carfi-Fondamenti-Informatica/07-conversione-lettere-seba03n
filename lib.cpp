93f18eb5b02e8e0fbe41138400323be00a3b6cc3
seba03n <115528575+seba03n@users.noreply.github.com>
2022-12-09 09:54:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+bool caratteri(char &x){
+    if(x >= 65 && x <= 90){
+        x += 32;
+        return true;
+    }else if(x >= 97 && x <= 122){
+        x -= 32;
+        return true;
+    }else
+        return false;
+}
