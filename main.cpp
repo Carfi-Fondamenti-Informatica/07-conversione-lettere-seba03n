c453ae1148307af0566b63eb9d94016db4c87faf
seba03n <115528575+seba03n@users.noreply.github.com>
2022-12-09 09:58:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char a=0;
+  cin >> a;
+  if(caratteri(a)){
+    cout<<a<<endl;
+  }else{
+    cout<<"errore"<<endl;
+  }
   return 0;
 }
